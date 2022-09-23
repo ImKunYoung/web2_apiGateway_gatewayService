c7b66b2d89856a4943165475d89633663438deab
ImKunYoung <lim8667@gmail.com>
2022-09-23 20:08:57 +0900

    init commit

---
+++ src/main/java/com/example/web2_apigateway_gatewayservice/Web2ApiGatewayGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.web2_apigateway_gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2ApiGatewayGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2ApiGatewayGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2_apigateway_gatewayservice/Web2ApiGatewayGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_apigateway_gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2ApiGatewayGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
